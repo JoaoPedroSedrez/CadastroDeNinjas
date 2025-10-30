321825d67d6e2633fbce600a1da7ff734cadf831
João Pedro Sedrez <sedrez.jp2@gmail.com>
2025-10-30 16:11:27 -0300

    Modelagem do Banco de Dados, NinjaModel e MissoesModel concluídos, anotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,46 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeMissao;
+    private String dificuldade;
+
+    // Uma missão para vários ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninja;
+
+    public MissoesModel() {
+    }
+
+    public MissoesModel(Long id, String nomeMissao, String dificuldade) {
+        this.id = id;
+        this.nomeMissao = nomeMissao;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getNomeMissao() {
+        return nomeMissao;
+    }
+
+    public void setNomeMissao(String nomeMissao) {
+        this.nomeMissao = nomeMissao;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    // Vários ninjas para uma missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreing Key = Chave Estrangeira
+    private MissoesModel missao;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
